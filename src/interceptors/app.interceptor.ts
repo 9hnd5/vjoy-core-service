2a884d81733ade16996b1ececb255ca586073521
phuctran <phuctran.ngh@gmail.com>
2023-02-02 09:55:48 +0700

    VVW-48 Global interceptor to format the response

---
+++ src/interceptors/app.interceptor.ts
@@ -0,0 +1,24 @@
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpException } from "@nestjs/common";
+import { Observable, throwError } from "rxjs";
+import { catchError, map } from "rxjs/operators";
+
+export interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class AppInterceptor<T> implements NestInterceptor<T, Response<T>> {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => ({ data })),
+      catchError((err) => throwError(() => {
+        return new HttpException(
+          {
+            error: (err instanceof Error || err instanceof TypeError) ? ({ code: err?.name || 'InternalServerException', message: err?.message || 'Something went wrong'}) : err,
+          },
+          err.status || 500
+        )}
+      ))
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { AppInterceptor } from "./interceptors/app.interceptor";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new AppInterceptor());
   await app.listen(3000);
 }
 bootstrap();
