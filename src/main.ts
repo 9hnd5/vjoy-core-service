db8eeb9071cdd1dcff78dbd51e10d3df1c9989e8
phuctran <phuctran.ngh@gmail.com>
2023-02-02 16:51:29 +0700

    VW-58 Install & implement Nest validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ResponseInterceptor } from "./interceptors/response.interceptor";
+import { ValidationPipe } from "./pipes/validation.pipe";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
