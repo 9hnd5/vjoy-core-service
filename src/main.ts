913efd85ed472f77eb6d58924eb671a58a54f6c0
phuctran <phuctran.ngh@gmail.com>
2023-02-03 16:05:36 +0700

    VW-58 update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from "@nestjs/common";
+import { UnprocessableEntityException, ValidationError, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ResponseInterceptor } from "./interceptors/response.interceptor";
@@ -6,7 +6,11 @@ import { ResponseInterceptor } from "./interceptors/response.interceptor";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory(errors: ValidationError[]) {
+      return new UnprocessableEntityException(errors);
+    },
+  }));
   await app.listen(3000);
 }
 bootstrap();
