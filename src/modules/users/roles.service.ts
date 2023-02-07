a231e205b61c520a4249fb5fe8474082570d3169
phuctran <phuctran.ngh@gmail.com>
2023-02-07 14:59:15 +0700

    VW-52 authorize, role api

---
+++ src/modules/users/roles.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/sequelize";
+import { Role } from "src/modules/auth/entities/role.entity";
+
+@Injectable()
+export class RolesService {
+  constructor(
+    @InjectModel(Role)
+    private roleModel: typeof Role,
+  ) {}
+
+  async findAll() {
+    const rs = await this.roleModel.findAll<Role>();
+    return rs;
+  }
+
+  async findOne(id: number) {
+    const rs = await this.roleModel.findByPk(id);
+    return rs;
+  }
+}
