b8d831b6ec81be1e591f09b0027ca3f4c769673b
9hnd5 <huy.nguyendinh@vus-etsc.edu.vn>
2023-02-09 16:47:29 +0700

    Add swagger document

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { UnprocessableEntityException, ValidationError, ValidationPipe } from "@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ResponseInterceptor } from "./interceptors/response.interceptor";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,14 @@ async function bootstrap() {
       },
     })
   );
+  const config = new DocumentBuilder()
+    .setTitle("Vjoy-Core")
+    .setDescription("The documentation vjoy-core")
+    .setVersion("1.0")
+    .addTag("vjoy-core")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
   await app.listen(3001);
 }
 bootstrap();
