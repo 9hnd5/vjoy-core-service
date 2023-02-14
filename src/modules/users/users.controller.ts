5ab2669d350bd36090df9cde9d8d38c7547e4505
phuctran <phuctran.ngh@gmail.com>
2023-02-14 14:43:58 +0700

    VW-69 first commit

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query, BadRequestException } from "@nestjs/common";
 import { Authorize } from "modules/auth/decorators/authorize.decorator";
 import { AdminOrSameUser } from "modules/auth/decorators/admin-or-same-user.decorator";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
+import { VerifyOTP } from "./dto/verify-otp.dto";
 
 @Controller("users")
 export class UsersController {
@@ -17,7 +18,7 @@ export class UsersController {
 
   @Authorize({ action: "list", resource: "users" })
   @Get()
-  findAll(@Query() query) {    
+  findAll(@Query() query) {
     return this.usersService.findAll(query);
   }
 
@@ -29,13 +30,22 @@ export class UsersController {
 
   @AdminOrSameUser()
   @Patch(":userId")
-  update(@Param("userId") userId: number, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(userId, updateUserDto);
+  async update(@Param("userId") userId: number, @Body() updateUserDto: UpdateUserDto) {
+    const rs = await this.usersService.update(userId, updateUserDto);
+    if (rs === -1) throw new BadRequestException("Email already exists");
+    if (rs === -2) throw new BadRequestException("Phone already exists");
+    if (rs === -3) throw new BadRequestException("Email or Phone already exists");
+    return rs;
   }
 
   @Authorize({ action: "delete", resource: "users" })
   @Delete(":userId")
   remove(@Param("userId") userId: number) {
     return this.usersService.remove(userId);
+  }
+
+  @Post("otp")
+  verifyOTP(@Body() data: VerifyOTP) {
+    return this.usersService.verifyOTP(data.otpCode, data.otpToken);
   }
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,1 @@
-import { SequelizeModuleOptions } from "@nestjs/sequelize";
-export const SEQUELIZE_OPTIONS = {
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT as string),
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_SCHEMA,
-  retryDelay: 5000,
-  retryAttempts: 3,
-  autoLoadModels: true,
-  logging: false,
-} satisfies SequelizeModuleOptions;
+export const OTP_TOKEN_EXPIRES = "5m";
