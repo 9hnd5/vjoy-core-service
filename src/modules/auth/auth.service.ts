82451e9730b562b93dd19d6c76eefd2582d55b10
Tran Nguyen Huu Phuc <phuctran@Vuss-MacBook-Air-3.local>
2023-03-13 17:23:07 +0700

    fix test

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -13,18 +13,19 @@ export class AuthService extends BaseService {
   private expiresIn: string;
   private secret: string;
   private atSecret: string;
+  private env: string;
   constructor(
     configService: ConfigService,
     private jwtService: JwtService,
     private smsService: SmsService,
     @InjectModel(User) private userModel: typeof User,
-    @InjectModel(Role) private roleModel: typeof Role,
     @InjectModel(ApiKey) private apiKeyModel: typeof ApiKey
   ) {
     super();
     this.expiresIn = configService.get("JWT_EXPIRES") || "";
     this.secret = configService.get("JWT_SECRET") || "";
     this.atSecret = configService.get("JWT_API_TOKEN_SECRET") || "";
+    this.env = configService.get("ENV")!;
   }
 
   login(data: LoginDto) {
@@ -132,8 +133,12 @@ export class AuthService extends BaseService {
     }
 
     const otpCode = this.generateOTPCode();
-    const smsContent = this.i18n.t("sms.OTP", { args: { otpCode, min: OTP_TOKEN_EXPIRES.replace("m", "") } });
-    this.smsService.send(userPhone, smsContent as string);
+
+    if (this.env != "test") {
+      const smsContent = this.i18n.t("sms.OTP", { args: { otpCode, min: OTP_TOKEN_EXPIRES.replace("m", "") } });
+      this.smsService.send(userPhone, smsContent as string);
+    }
+
     return { otpToken: await this.generateOTPToken(otpCode, payload) };
   }
 }
