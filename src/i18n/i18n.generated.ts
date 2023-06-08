67d2f32058381861f537f871a38bb0de707652d8
9hnd5 <huy.nguyendinh@vus-etsc.edu.vn>
2023-06-08 15:53:37 +0700

    Refactor i18n folder

--- src/i18n/i18n.generated.ts
+++ src/i18n/i18n.generated.ts
@@ -2,25 +2,7 @@
 
 import { Path } from "nestjs-i18n";
 export type I18nTranslations = {
-    "email": {
-        "OTP_SUBJECT": string;
-        "OTP_BODY": string;
-        "NEW_ACCOUNT_SUBJECT": string;
-        "NEW_ACCOUNT_BODY": string;
-        "SIGNUP_ACCOUNT_SUBJECT": string;
-        "SIGNUP_ACCOUNT_BODY": string;
-        "RESET_PASSWORD_SUBJECT": string;
-        "RESET_PASSWORD_BODY": string;
-    };
-    "message": {
-        "EMAIL_EXISTS": string;
-        "GOOGLE_NOT_FOUND": string;
-        "INVALID_CREDENTIAL": string;
-        "INVALID_TOKEN": string;
-        "IS_EXISTED": string;
-        "NOT_PERMISSION": string;
-        "NOT_FOUND": string;
-        "PHONE_EXISTS": string;
+    "auth": {
         "REQUEST_LIMITED": string;
         "REQUEST_RESET_PASSWORD_SUCCESSFUL": string;
         "REQUEST_TOO_FAST": string;
@@ -33,9 +15,22 @@ export type I18nTranslations = {
         "USER_EXISTED": string;
         "USER_REGISTRATION_SUCCESSFUL": string;
         "USER_RESEND_REGISTRATION_SUCCESSFUL": string;
-    };
-    "sms": {
+        "INVALID_CREDENTIAL": string;
         "OTP": string;
+        "OTP_SUBJECT": string;
+        "OTP_BODY": string;
+        "NEW_ACCOUNT_SUBJECT": string;
+        "NEW_ACCOUNT_BODY": string;
+        "SIGNUP_ACCOUNT_SUBJECT": string;
+        "SIGNUP_ACCOUNT_BODY": string;
+        "RESET_PASSWORD_SUBJECT": string;
+        "RESET_PASSWORD_BODY": string;
+    };
+    "message": {
+        "INVALID": string;
+        "IS_EXISTED": string;
+        "NOT_PERMISSION": string;
+        "NOT_FOUND": string;
     };
 };
 export type I18nPath = Path<I18nTranslations>;
